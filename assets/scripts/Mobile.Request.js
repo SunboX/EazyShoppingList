34f22083bf3b4731853a9eca7cef7616b9176522
SunboX <fiedler.andre@gmail.com>
2009-11-30 18:02:14 +0100

    some cleanup and improvements in Mobile.Request.Screen

--- assets/scripts/Mobile.Request.js
+++ assets/scripts/Mobile.Request.js
@@ -10,14 +10,11 @@ Mobile.Request = Mobile.Request || {};
 
 Mobile.Request.Screen = new Class({
 
-	Implements: [Chain, Events, Options, Log],
+	Implements: [Events, Options, Log],
 
 	options: {
 		url: '',
-		data: {},
-		retries: 0,
-		timeout: 0,
-		link: 'ignore',
+		noCache: false,
 		injectScript: document.head
 	},
 
@@ -25,79 +22,51 @@ Mobile.Request.Screen = new Class({
 		this.setOptions(options);
 		if (this.options.log) this.enableLog();
 		this.running = false;
-		this.requests = 0;
-		this.triesRemaining = [];
+		this.toLoad = [];
 	},
-
-	check: function(){
-		if (!this.running) return true;
-		switch (this.options.link){
-			case 'cancel': this.cancel(); return true;
-			case 'chain': this.chain(this.caller.bind(this, arguments)); return false;
-		}
-		return false;
-	},
-
-	send: function(options){
-		if (!$chk(arguments[1]) && !this.check(options)) return this;
-
-		var type = $type(options), 
-				old = this.options, 
-				index = $chk(arguments[1]) ? arguments[1] : this.requests++;
-		if (type == 'string' || type == 'element') options = {data: options};
-
-		options = $extend({data: old.data, url: old.url}, options);
-
-		if (!$chk(this.triesRemaining[index])) this.triesRemaining[index] = this.options.retries;
-		var remaining = this.triesRemaining[index];
-
-		(function(){
-			var script = this.getScript(options);
-			this.log('Mobile.Request.Screen retrieving script with url: ' + script.get('src'));
-			this.fireEvent('request', script);
-			this.running = true;
-
-			(function(){
-				if (remaining){
-					this.triesRemaining[index] = remaining - 1;
-					if (script){
-						script.destroy();
-						this.send(options, index).fireEvent('retry', this.triesRemaining[index]);
-					}
-				} else if(script && this.options.timeout){
-					script.destroy();
-					this.cancel().fireEvent('failure');
-				}
-			}).delay(this.options.timeout, this);
-		}).delay(Browser.Engine.trident ? 50 : 0, this);
+	
+	send: function(options) {
+		options = $merge(this.options, options);
+		if (this.toLoad.length) return this.loadScript(this.toLoad.shift());
+		if (this.running) return this.toLoad.push(script);
+		this.running = true;
+		this.script = new Element('script', {
+			src: options.url + (this.options.noCache ? '?noCache=' + $time() : ''),
+			events: {
+				load: function() {
+					this.success();
+				}.bind(this),
+				error: function() {
+					this.error();
+				}.bind(this)
+			}
+		}).inject(options.injectScript || document.head);
 		return this;
 	},
 
 	cancel: function(){
 		if (!this.running) return this;
+		if ($chk(this.script)) this.script.destroy();
 		this.running = false;
 		this.fireEvent('cancel');
 		return this;
-	},
-
-	getScript: function(options){
-		var index = Mobile.Request.Screen.counter;
-		Mobile.Request.Screen.counter++;
-
-		var src = options.url;
-		if (src.length > 2083) this.log('Mobile.Request.Screen '+ src +' will fail in Internet Explorer, which enforces a 2083 bytes length limit on URIs');
-
-		this.script = new Element('script', {type: 'text/javascript', src: src});
-		return this.script.inject(this.options.injectScript);
 	},
 
 	success: function(){
 		if ($chk(this.script)) this.script.destroy();
 		this.running = false;
 		this.log('Mobile.Request.Screen successfully loaded.');
-		this.fireEvent('complete').fireEvent('success').callChain();
+		this.fireEvent('success');
+		if (this.toLoad.length) this.loadScript(this.toLoad.shift());
+		return this;
+	},
+
+	error: function(){
+		if ($chk(this.script)) this.script.destroy();
+		this.running = false;
+		this.log('Mobile.Request.Screen could not load script.');
+		this.fireEvent('error');
+		if (this.toLoad.length) this.loadScript(this.toLoad.shift());
+		return this;
 	}
-
 });
-
-Mobile.Request.Screen.counter = 0;
