721dd1fa1e54c95fedca3a7db736f91d1bb0e0a9
SunboX <fiedler.andre@gmail.com>
2009-12-05 20:40:18 +0100

    changed license

--- service/app/actions/registration.php
+++ service/app/actions/registration.php
@@ -1,12 +1,12 @@
 <?
 /**
- * content adicted CMS
+ * EazyShoppingList Service Application
  *
- * @author     André Fiedler <kontakt at visualdrugs dot net>
- * @link       http://github.com/SunboX/content-adicted/tree
+ * @author     Dipl.-Ing. (FH) André Fiedler <kontakt at visualdrugs dot net>
+ * @link       http://github.com/SunboX/EazyShoppingList
  * @copyright  2007 - 2009 André Fiedler.
- * @license    MIT License
- * @version    2.0
+ * @license    http://creativecommons.org/licenses/by-nc-nd/3.0/
+ * @version    0.1
  */
  
 class Registration_Action implements IContentAdictedAction
--- service/index.php
+++ service/index.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * EazyShoppingList Service Application
+ *
+ * @author     Dipl.-Ing. (FH) André Fiedler <kontakt at visualdrugs dot net>
+ * @link       http://github.com/SunboX/EazyShoppingList
+ * @copyright  2007 - 2009 André Fiedler.
+ * @license    http://creativecommons.org/licenses/by-nc-nd/3.0/
+ * @version    0.1
+ */
 
 require_once('lib/ContentAdicted.php');
 require_once('lib/Util.php');
